8999738fdab43ff82e9eb7c946e03cac85888ce7
PhamAnhHoang <hoangpa@paditech.com>
2021-07-29 13:44:17 +0700

    Update power event

--- orm/document/tx.go
+++ orm/document/tx.go
@@ -156,16 +156,20 @@ func (_ CommonTx) GetListTxBy(size int) ([]CommonTx, error) {
 
 func (_ CommonTx) GetListTxByAddress(before int, size int, operatorAddress string) ([]CommonTx, error) {
 	var data []CommonTx
-	query := bson.M{"messages": bson.RegEx{operatorAddress + ".*", ""}}
-	typeArr := []string{TypeDelegate, TypeUnBond, TypeReDelegate}
-	query[Tx_Field_Type] = bson.M{
-		"$in": typeArr,
-	}
+
+	query := bson.M{}
 	if before != 0 {
 		query[Tx_Field_Height] = bson.M{
 			"$lt": before,
 		}
 	}
+
+	typeArr := []string{TypeDelegate, TypeUnBond, TypeReDelegate}
+	query[Tx_Field_Type] = bson.M{
+		"$in": typeArr,
+	}
+
+	query["messages"] = bson.RegEx{operatorAddress + ".*", ""}
 
 	err := querylistByOffsetAndSize(CollectionNmCommonTx, nil, query, desc(Tx_Field_Time), 0, size, &data)
 	return data, err
